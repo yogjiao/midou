6c72feb774aba44e618968db1aea22eb3ae61cc7
bugknightyyp <bugknightyyp@gmail.com>
2016-03-30 15:25:50 +0800

    binding data for Collocation

--- routers/Collocation/components/Collocation.js
+++ routers/Collocation/components/Collocation.js
@@ -6,10 +6,13 @@ import PageHeader from 'PageHeader/PageHeader.js'
 import {
   BASE_PAGE_DIR,
   BASE_STATIC_DIR,
-  FETCH_MATCH_GOODS
+  FETCH_MATCH_GOODS,
+  FETCH_SUCCESS
 } from 'macros.js'
+import PageSpin from 'PageSpin/PageSpin.js'
 import {fetchable, fetchAuth} from 'fetch.js'
 import {backToNativePage} from 'webviewInterface.js'
+import {getParentByClass} from 'util.js'
 // import fetch from '../../components/fetch.js'
 import Swiper from 'Swiper'
 
@@ -19,15 +22,52 @@ class Collocation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      headerName: '搭配组合'
+      headerName: '',
+      isHiddenPageSpin: false,
+      isHiddenSharePanel: true,
+      match: {},
+      goods: []
     }
     //this.state.tagsIndex[0] = 0
   }
   fetchData = () => {
-    
+    let url = `${FETCH_MATCH_GOODS}/${this.props.params.collocationId}`
+    fetchable(url)
+      .then((data) => {
+        if (data.rea == FETCH_SUCCESS) {
+          data.isHiddenPageSpin = true
+          this.setState(data, () => {
+            this.swiper.updateSlidesSize()
+            this.swiper.updatePagination()
+          })
+        } else {
+
+        }
+
+      })
+      .catch(() => {
+        this.setState({isHiddenPageSpin: true})
+      })
+  };
+  thisHandler = (e) => {
+    let target
+    if (target = getParentByClass(e.target, 'icon-arrow-left')) {
+      this.props.history.goBack()
+    } else if (target = getParentByClass(e.target, 'icon-share')) {
+      nextState.isHiddenSharePanel = false
+    }
+  };
+  formatTime = (seconds) => {
+    let oDate = new Date(seconds * 1000)
+    let y = oDate.getFullYear()
+    let m = oDate.getMonth() + 1
+    let d = oDate.getDate()
+    return `${y}/${m}/${d}`
   };
   componentDidMount = () => {
-    var swiper = new Swiper('.swiper-container', {
+    this.fetchData()
+
+    this.swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         slidesPerView: 1,
         paginationClickable: true,
@@ -36,58 +76,54 @@ class Collocation extends React.Component {
     });
   };
   render() {
+    let h = window.innerHeight;
     return (
-      <div className="collocation-container">
+        <div className="collocation-container" onClick={this.thisHandler}>
         <PageHeader headerName={this.state.headerName}>
-          <div></div>
-          <a className="menu-search" href={`${BASE_PAGE_DIR}/search`}>所有单品</a>
+          <div className="iconfont icon-arrow-left"></div>
+          <div className="iconfont icon-share"></div>
         </PageHeader>
-        <div className="swiper-container">
-          <div className="swiper-wrapper">
-              <div className="swiper-slide">
-                <div className="mix-cover">
-                  <h6>最亲密的也要分散</h6>
-                  <div className="mix-intro">
-                        依偎在松软的被窝里，小夜灯也散着暖暖的
-                        黄临睡的那一秒，接管脑袋的潜意识忽然喃
-                        喃道现在的生活你可还喜欢？只答“喜欢”好
-                        像过于草率，干脆在心中陈列这一天，从黎
-                        明到傍晚从晨起的新衣到夜灯旁的庆山集，
-                        嗯，可以安心入眠了。
-                  </div>
-                  <div className="mix-time">2016/05/03</div>
-                </div>
-              </div>
-              <div className="swiper-slide">
-                <div className="mix-item-wrap">
-                  <div className="img-wrap">
-                    <div style={{backgroundImage: `url(${BASE_STATIC_DIR}/img/test.png)`}}/>
-                  </div>
-                  <div className="mix-intro">
-                    <div className="mix-adjust">
-                    <p>
-                        依偎在松软的被窝里，小夜灯也散着暖暖的
-                        黄临睡的那一秒，接管脑袋的潜意识忽然喃
-                        喃道现在的生活你可还喜欢？只答“喜欢”好
-                        像过于草率，干脆在心中陈列这一天，从黎
-                        明到傍晚从晨起的新衣到夜灯旁的庆山集，
-                        嗯，可以安心入眠了。
-                        依偎在松软的被窝里，小夜灯也散着暖暖的
-                        黄临睡的那一秒，接管脑袋的潜意识忽然喃
-                        喃道现在的生活你可还喜欢？只答“喜欢”好
-                        像过于草率，干脆在心中陈列这一天，从黎
-                        明到傍晚从晨起的新衣到夜灯旁的庆山集，
-                        嗯，可以安心入眠了。
-                    </p>
+        <div className="collocation-wrap" style={{backgroundImage: `url(${this.state.match.match_img})`, height: h}}>
+          <div className="overlayer">
+            <div className="swiper-container">
+
+                <div className="swiper-wrapper">
+                    <div className="swiper-slide">
+                      <div className="mix-cover">
+                        <h6>{this.state.match.match_title}</h6>
+                        <div className="mix-intro">{this.state.match.match_intro}
+                        </div>
+                        <div className="mix-time">{this.formatTime(this.state.match.ts)}</div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="iconfont icon-collection" />
+                    {
+                      this.state.goods.map((item, index) => {
+                        return (
+                          <div className="swiper-slide" key={index}>
+                            <div className="mix-item-wrap">
+                              <div className="img-wrap">
+                                <div style={{backgroundImage: `url(${item.thumb_img})`}}/>
+                              </div>
+                              <div className="mix-intro">
+                                <div className="mix-adjust">
+                                  <p>{item.intro}</p>
+                                </div>
+                              </div>
+                              <div className="iconfont icon-collection" />
+                            </div>
+                          </div>
+                        )
+                      })
+                    }
                 </div>
-              </div>
-              <div className="swiper-slide"> 3</div>
+                <div className="swiper-pagination"></div>
+            </div>
           </div>
-          <div className="swiper-pagination"></div>
         </div>
+        <ShareToSocialMedia
+          isHidden={this.state.isHiddenSharePanel}
+        />
+        <PageSpin isHidden={this.state.isHiddenPageSpin}/>
       </div>
     )
   }
