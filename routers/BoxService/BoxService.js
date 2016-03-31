8bfe5d86916e5119273ebf89902bf344ba9ed8dc
bugknightyyp <bugknightyyp@gmail.com>
2016-03-31 13:38:37 +0800

    refactor BoxService in slider way

--- routers/BoxService/BoxService.js
+++ routers/BoxService/BoxService.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import {BASE_STATIC_DIR} from 'macros.js'
+import {
+  BASE_STATIC_DIR,
+  BASE_PAGE_DIR
+} from 'macros.js'
 import {backToNativePage} from 'webviewInterface.js'
+import Swiper from 'Swiper'
 
 import './BoxService.less'
 class BoxService extends React.Component {
@@ -11,42 +15,61 @@ class BoxService extends React.Component {
       })
     this.props.history.goBack();
   };
+  componentDidMount = () => {
+    var swiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      paginationClickable: true
+    })
+  };
   render = () => {
+    let h = window.innerHeight
     return (
       <div className="box-servie-container">
         <i className="iconfont icon-arrow-left" onClick={this.backHandler}></i>
-        <div className="img-wrap">
-          <img src={`${BASE_STATIC_DIR}/img/box-describe-1.png`} />
+        <div className="swiper-container" style={{height: h}}>
+          <div className="swiper-wrapper">
+              <div className="swiper-slide">
+                <div className="box-service-wrap">
+                  <h2>免费试衣</h2>
+                  <div className="img-wrap">
+                    <div style={{backgroundImage: `url(${BASE_STATIC_DIR}/img/box-1.png)`}}/>
+                  </div>
+                  <h6>购买1件送达2件</h6>
+                  <p className="tips-wrap">
+                    每选购一件内衣，您可以<br />免费试穿一件相邻码数的内衣
+                  </p>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="box-service-wrap">
+                  <h2>全程包邮</h2>
+                  <div className="img-wrap">
+                    <div style={{backgroundImage: `url(${BASE_STATIC_DIR}/img/box-2.png)`}}/>
+                  </div>
+                  <h6>抵¥60返¥70</h6>
+                  <p className="tips-wrap">
+                    结账时需暂时放入60元试穿基金，<br />
+被试穿的内衣成功退回后，<br />
+返程邮费与试穿基金将一起返还至您的账户
+                  </p>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="box-service-wrap">
+                  <h2>诚信得好礼</h2>
+                  <div className="img-wrap">
+                    <div style={{backgroundImage: `url(${BASE_STATIC_DIR}/img/box-3.png)`}}/>
+                  </div>
+                  <h6>升级盒子</h6>
+                  <p className="tips-wrap">
+                    及时返还内衣能够提高信用记录<br />
+帮助解锁更高级别的试穿盒子
+                  </p>
+                </div>
+              </div>
+          </div>
+          <div className="swiper-pagination"></div>
         </div>
-        <div className="box-item-wrap">
-           <h5>购买1件送达2件</h5>
-           <div className="img-wrap">
-             <img src={`${BASE_STATIC_DIR}/img/box-describe-2.png`} />
-           </div>
-           <p>每购买一件内衣，您可以选择一次试穿盒子
-服务，多试穿一件推荐的相邻码数内衣。试
-穿后您可以留下两件中最合身的一件，并进
-行“返还内衣”完成交易。</p>
-        </div>
-        <div className="box-item-wrap">
-           <h5>试六十返七十</h5>
-           <div className="img-wrap">
-             <img src={`${BASE_STATIC_DIR}/img/box-describe-3.png`} />
-           </div>
-           <p>内新提供您试衣所需的往返邮费，交易完成
-后，您购买时抵试的60元试穿诚信金，将增
-加至70元原路返回到支付账户。</p>
-        </div>
-        <div className="box-item-wrap">
-           <h5>及时返还内衣得诚信好礼</h5>
-           <div className="img-wrap">
-             <img src={`${BASE_STATIC_DIR}/img/box-describe-4.png`} />
-           </div>
-           <p>您使用的盒子服务记录，将被内新认真保存
-下来，信用记录良好的姑娘，将能解锁更高
-级别的试穿盒子。</p>
-        </div>
-        <div className="img-wrap logo-wrap"><img src={`${BASE_STATIC_DIR}/img/neixin.png`} /></div>
       </div>
     )
   };
